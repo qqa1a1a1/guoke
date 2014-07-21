7be481c782380ba5209bfd7e1ceffa16ffe585ef
zimo jin <631930955@qq.com>
2014-07-22 02:59:51 +0800

    new change
    try a new UI

---
+++ js/init.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	// Routing
+	dispatch.init();
+
+	/*
+	var stats = new Stats();
+
+	// Align top-left
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.left = '0px';
+	stats.domElement.style.top = '0px';
+
+	document.getElementById("container").appendChild( stats.domElement );
+
+	setInterval( function () {
+
+	    stats.update();
+
+	}, 1000 / 60 );
+	*/
+	
+});
